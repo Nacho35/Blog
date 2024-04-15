97c6661ba6a5e9e9dbe976e99ad0a25ab9c04132
Admin <ignacioevil@gmail.com>
2024-04-14 22:30:01 -0300

    modificando estilos a las card

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -37,27 +37,26 @@ const Home = () => {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center m-auto p-5 lg:flex-row lg:flex lg:justify-between">
+    <main className="flex min-h-screen flex-col m-auto p-4 lg:flex-row lg:flex lg:justify-between">
       {posts.map((post) => (
         <Card
           key={post.id}
-          className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-5"
+          className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-5 m-auto"
           renderImage={() => (
             <img
-              width={500}
-              height={500}
+              className="w-full h-64 object-cover"
               src={post.attributes.imageUrl}
               alt="cover"
             />
           )}
         >
           <Link href={`/post/${post.id}`}>
-            <div className="m-auto  ">
+            <div className="m-auto flex-grow">
               <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-pretty">
                 {post.attributes.title}
               </h5>
             </div>
-            <div>
+            <div className="flex-grow">
               <p className="text-sm font-normal text-yellow-700 dark:text-gray-400 my-4 capitalize">
                 {new Date(post.attributes.created).toLocaleString("es-ES", {
                   weekday: "long",
@@ -69,7 +68,7 @@ const Home = () => {
                 })}
               </p>
             </div>
-            <div>
+            <div className="flex-grow">
               <p className="font-normal text-balance text-gray-700 dark:text-gray-400">
                 {post.attributes.content}
               </p>
