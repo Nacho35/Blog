1cf67be136398b1a7cf7ef9e358751440b93628c
nacho35 <ignacioevil@gmail.com>
2024-05-24 00:08:05 -0300

    Register terminado 👏️

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,10 +1,21 @@
-import { Box, Button, Container, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useRouter } from "next/router";
 import { useState } from "react";
+import { Toaster, toast } from "react-hot-toast";
 
 const RegisterForm = () => {
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const router = useRouter();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -25,101 +36,140 @@ const RegisterForm = () => {
         },
       );
 
-      if (!response.ok) {
-        throw new Error("Error al registrarse");
+      if (response.ok) {
+        toast.success("Registro exitoso Ahora puedes iniciar sesión.");
+        setTimeout(() => {
+          router.push("/login");
+        }, 4000);
+      } else {
+        const errorData = await response.json();
+        toast.error(
+          `Hubo un error durante el registro: ${errorData.message || "Por favor, inténtalo de nuevo."}`,
+        );
       }
-
-      const data = await response.json();
-      console.log(data); // Aquí puedes manejar la respuesta, por ejemplo, redirigir al usuario a otra página o mostrar un mensaje de éxito
     } catch (error) {
-      console.error(error);
-      // Manejar el error, por ejemplo, mostrar un mensaje de error
+      toast.error(
+        "Hubo un error durante el registro. Por favor, inténtalo de nuevo.",
+      );
     }
   };
 
   return (
     <Container
-      maxWidth="false"
       sx={{
-        backgroundImage: "url(/palms.jpg)",
-        objectFit: "cover",
-        backgroundPosition: "center",
         height: "100vh",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
         overflow: "hidden",
       }}
     >
-      <form
-        style={{
+      <Container
+        maxWidth="sm"
+        sx={{
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
-          height: "100vh",
-          width: "100%",
+          alignItems: "center",
+          borderRadius: "10px",
+          boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+          border: "1px solid #333",
+          backgroundColor: "#F0EADA",
+          maxWidth: "500px",
+          maxHeight: "500px",
+          py: 5,
+          margin: "auto",
         }}
-        onSubmit={handleSubmit}
       >
-        <h2
-          style={{
-            fontSize: "2rem",
-            fontWeight: "bold",
-            textAlign: "center",
-            marginBottom: "20px",
-            textTransform: "capitalize",
+        <FormControl
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            width: "100%",
           }}
+          onSubmit={handleSubmit}
         >
-          ¡Bienvenido! crea tu cuenta
-        </h2>
-        <Box sx={{ display: "flex", justifyContent: "center" }}>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              width: "50%",
-            }}
-          >
-            <TextField
-              label="Correo Electrónico"
-              variant="outlined"
-              fullWidth
-              margin="normal"
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-              label="Nombre de Usuario"
-              variant="outlined"
-              fullWidth
-              margin="normal"
-              required
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <TextField
-              label="Contraseña"
-              variant="outlined"
-              fullWidth
-              margin="normal"
-              required
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+          <Box>
+            <Typography
+              variant="h4"
+              sx={{
+                textAlign: "center",
+                fontWeight: "600",
+                mb: 2,
+                fontFamily: "Roboto, sans-serif",
+              }}
+            >
+              Blog Codigo & Cafe
+            </Typography>
+            <Typography
+              sx={{
+                textAlign: "center",
+                fontFamily: "Roboto, sans-serif",
+                mb: 2,
+              }}
+            >
+              Crea una cuenta gratis o
+              <Link sx={{ ml: 1 }} href="http://localhost:3000/login">
+                inicia sesión
+              </Link>
+            </Typography>
           </Box>
-        </Box>
-        <Box sx={{ display: "flex", justifyContent: "center" }}>
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            width="50%"
-            sx={{ marginTop: "20px" }}
-          >
-            Registrarse
-          </Button>
-        </Box>
-      </form>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                width: "80%",
+              }}
+            >
+              <TextField
+                label="Correo Electrónico"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                label="Nombre de Usuario"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                required
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <TextField
+                label="Contraseña"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                required
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Box>
+          </Box>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              width="50%"
+              sx={{ marginTop: "20px" }}
+              onClick={handleSubmit}
+            >
+              Registrarse
+            </Button>
+            <Toaster position="top-center" reverseOrder={false} />
+          </Box>
+        </FormControl>
+      </Container>
     </Container>
   );
 };
